00ff40be8ac48b283c2e156d6e15b9db668cd82a
PJ1nho <paveltihtey@gmail.com>
2024-01-14 20:58:57 +0300

    error fixed

---
+++ DeliveryApp/DeliveryApp/Models /Dish.swift
@@ -0,0 +1,14 @@
+//
+//  Dishes.swift
+//  DeliveryApp
+//
+//  Created by Тихтей  Павел on 14.01.2024.
+//
+
+import Foundation
+
+struct Dish: Decodable {
+    let name: String?
+    let description: String?
+    let price: Int?
+}
